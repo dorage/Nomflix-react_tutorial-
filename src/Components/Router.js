fca55abbb44b5f983f16216e1f76e28357f6c29b
dorage <baloonflower554@gmail.com>
2019-05-19 13:47:24 +0900

    edt routes

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -17,10 +17,10 @@ export default () => (
       <Header />
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/tv" exact component={TV} />
-        <Route path="/search" exact component={Search} />
-        <Route path="/movie/:id" exact component={Detail} />
-        <Route path="/show/:id" exact component={Detail} />
+        <Route path="/tv" component={TV} />
+        <Route path="/search" component={Search} />
+        <Route path="/movie/:id" component={Detail} />
+        <Route path="/show/:id" component={Detail} />
         <Redirect from="*" to="/" />
       </Switch>
     </>
